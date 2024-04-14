e9da1c00abac538c7437ef624ae1bbf21fcec755
danojeda <daniel.ojeda.escobar@gmail.com>
2024-04-13 21:13:39 -0400

    correccion json corto

--- scripts/project-script.js
+++ scripts/project-script.js
@@ -3,129 +3,7 @@
 
 
 const questionsElement = document.querySelector("div");
-let questionList = [];
-
-
-
-
-let displayQuestions = (questions) =>{
-
-    for (i=0;i < questions.length; i++){
-        console.log(questions[i]);
-        myQ = document.createElement("h1");
-        
-        myOption1 = document.createElement("input");
-        myOption2 = document.createElement("input");
-        myOption3 = document.createElement("input");
-        myOption4 = document.createElement("input");    
-
-        myOption1.type="radio";
-        myOption2.type="radio";
-        myOption3.type="radio";
-        myOption4.type="radio";
-
-        myOption1.name="option-q";
-        myOption2.name="option-q";
-        myOption3.name="option-q";
-        myOption4.name="option-q";
-
-        myOption1.id="Opt-A";
-        myOption2.id="Opt-B";
-        myOption3.id="Opt-C";
-        myOption4.id="Opt-D";
-
-        let textNode = document.createElement(questions.Q);
-       
-        myQ.appendChild(textNode);
-        myOption1.value=questions[i].alternatives[0];
-        myOption2.value=questions[i].alternatives[1];
-        myOption3.value=questions[i].alternatives[2];
-        myOption4.value=questions[i].alternatives[3];
-
-        myLabelZero= document.createElement("label");
-        myLabel1= document.createElement("label");
-        myLabel2= document.createElement("label");
-        myLabel3= document.createElement("label");
-        myLabel4= document.createElement("label");
-
-        myLabelZero.textContent= questions[i].Q;
-        myLabel1.textContent= questions[i].alternatives[0];
-        myLabel2.textContent= questions[i].alternatives[1];
-        myLabel3.textContent= questions[i].alternatives[2];
-        myLabel4.textContent= questions[i].alternatives[3];
-
-        
-        
-        let myBR = document.createElement('br');
-        let myBR2 = document.createElement('br');
-        let myBR3 = document.createElement('br');
-        let myBR4 = document.createElement('br');
-        let myBR5 = document.createElement('br');
-        let myBR6 = document.createElement('br');
-        let myBR7 = document.createElement('br');
-
-        let myButton = document.createElement("button");
-        myButton.innerText=`Submit`;
-        
-        
-
-        questionsElement.appendChild(myLabelZero);
-        questionsElement.appendChild(myBR3);
-        questionsElement.appendChild(myBR6);
-        questionsElement.appendChild(myOption1);
-        questionsElement.appendChild(myLabel1);
-        questionsElement.appendChild(myBR);
-        questionsElement.appendChild(myOption2);
-        questionsElement.appendChild(myLabel2);
-        questionsElement.appendChild(myBR2);
-        questionsElement.appendChild(myOption3);
-        questionsElement.appendChild(myLabel3);
-        questionsElement.appendChild(myBR4);
-        questionsElement.appendChild(myOption4);
-        questionsElement.appendChild(myLabel4);
-        questionsElement.appendChild(myBR5);
-        questionsElement.appendChild(myButton);
-        questionsElement.appendChild(myBR6);
-
-        myButton.addEventListener("click",function () {calculateFunction(this)});
-
-        function calculateFunction  () {
-    
-            if (myOption1.checked){
-                
-                alert(`You have selected the first option`);
-            }else if (myOption2.checked){
-                alert(`You have selected the second option`);
-            }else if (myOption3.checked){
-                alert(`You have selected the third option`);
-            }else if (myOption4.checked){
-                alert(`You have selected the fourth option`);
-            }
-           
-             
-        }
-              
-
-    }
-}
-
-
-
-function calculateFunction  () {
-    
-    if (myOption1.checked){
-        
-        alert(`You have selected the first option`);
-    }else if (myOption2.checked){
-        alert(`You have selected the second option`);
-    }else if (myOption3.checked){
-        alert(`You have selected the third option`);
-    }else if (myOption4.checked){
-        alert(`You have selected the fourth option`);
-    }
-   
-     
-}
+let questionsList = [];
 
 
 const getQuestions = async () =>{
@@ -142,6 +20,164 @@ const getQuestions = async () =>{
 }
 
 
+
+
+
+
+let displayQuestions = (questions) =>{
+
+    
+    //    console.log(questions[i]);
+    let myQuestion = document.getElementById("myQuestion1");
+    myQuestion.innerText = questions[0].Q;
+    let myOption1 = document.getElementById("myQ1Option1");
+    let myOption2 = document.getElementById("myQ1Option2");
+    let myOption3 = document.getElementById("myQ1Option3");
+    let myOption4 = document.getElementById("myQ1Option4");
+              
+    let myLabel1 = document.getElementById("myQ1Label1");
+    /*console.log(questions[0].alternatives[0]);
+    console.log(questions[0].alternatives[1]);
+    console.log(questions[0].alternatives[2]);
+    console.log(questions[0].alternatives[3]);*/
+    myLabel1.innerHTML = questions[0].alternatives[0];
+    myOption1.alt = questions[0].alternatives[0];
+    console.log(myOption1.alt);
+        
+    let myLabel2 = document.getElementById("myQ1Label2");
+    myLabel2.innerHTML = questions[0].alternatives[1];
+    myOption2.alt = questions[0].alternatives[1];
+    console.log(myOption2.alt);
+    //myOption1.innerText=questions[0].
+
+    let myLabel3 = document.getElementById("myQ1Label3");
+    myLabel3.innerHTML = questions[0].alternatives[2];
+    myOption3.alt = questions[0].alternatives[2];
+    console.log(myOption3.alt);
+
+    let myLabel4 = document.getElementById("myQ1Label4");
+    myLabel4.innerHTML = questions[0].alternatives[3];
+    myOption4.alt = questions[0].alternatives[3];
+    console.log(myOption4.alt);
+
+    let myCorrect1 = questions[0].Correct;
+    console.log(myCorrect1);
+
+
+    //second question:
+
+    let myQuestion2 = document.getElementById("myQuestion2");
+    myQuestion2.innerText = questions[1].Q;
+    let myQ2Option1 = document.getElementById("myQ2Option1");
+    let myQ2Option2 = document.getElementById("myQ2Option2");
+    let myQ2Option3 = document.getElementById("myQ2Option3");
+    let myQ2Option4 = document.getElementById("myQ2Option4");
+              
+    let myQ2Label1 = document.getElementById("myQ2Label1");
+    //console.log(questions[0].alternatives[0]);
+    //console.log(questions[0].alternatives[1]);
+    //console.log(questions[0].alternatives[2]);
+    //console.log(questions[0].alternatives[3]);
+    myQ2Label1.innerHTML = questions[1].alternatives[0];
+        
+    let myQ2Label2 = document.getElementById("myQ2Label2");
+    myQ2Label2.innerHTML = questions[1].alternatives[1];
+
+    let myQ2Label3 = document.getElementById("myQ2Label3");
+    myQ2Label3.innerHTML = questions[1].alternatives[2];
+
+    let myQ2Label4 = document.getElementById("myQ2Label4");
+    myQ2Label4.innerHTML = questions[1].alternatives[3];
+
+    //let mySubmit = document.getElementById("mySubmitDef");
+    //mySubmit = addEventListener("click",function() {calculateFunction(questions)});
+
+   
+    
+}
+
+/*
+
+function calculateFunction  () {
+    
+    if (myOption1.checked){
+        
+        alert(`You have selected the first option`);
+        console.log("selected q1 a1");
+        //console.log("");
+    }else if (myOption2.checked){
+        alert(`You have selected the second option`);
+        console.log("selected q1 a2");
+    }else if (myOption3.checked){
+        alert(`You have selected the third option`);
+        console.log("selected q1 a3");
+    }else if (myOption4.checked){
+        alert(`You have selected the fourth option`);
+        console.log("selected q1 a4");
+    }
+   
+     
+}*/
+
+/** var type = document.querySelector('input[name="type"]:checked').value
+
+console.log(type); */
+
+
+let calculateFunction =async  (questions)=> {
+    console.log("llegue a la funcion");
+    const response = await fetch ("https://danojeda.github.io/cse121b/questionsandanswers.json");
+    if (response.ok){
+        const data = await response.json();
+        questionsList = data;
+    } 
+
+
+    //let myCorrect1 = 
+    let myElement = document.getElementsByName("Alternatives1");
+    console.log("declare mi myElement");
+    //if (myElement.myOption1.checked){
+    if (myElement[0].checked){
+        
+        //alert(`You have selected the first option`);
+        console.log("selected q1 a1");
+        //console.log(myElement[0].);
+        console.log(myElement[0].alt);
+        if (myElement[0].alt  == questionsList[0].correct){
+            console.log("la 1 es correcta");
+        }
+        //console.log("");
+    }else if (myElement[1].checked){
+        //alert(`You have selected the second option`);
+        console.log("selected q1 a2");
+    }else if (myElement[2].checked){
+        //alert(`You have selected the third option`);
+        console.log("selected q1 a3");
+    }else if (myElement[3].checked){
+        //alert(`You have selected the fourth option`);
+        console.log("selected q1 a4");
+    }
+
+    //console.log(questions);
+    let myElement2 = document.getElementsByName("Alternatives1");
+    if (myElement2[0].checked){
+        
+        //alert(`You have selected the first option`);
+        console.log("selected q2 a1");
+        //console.log("");
+    }else if (myElement2[1].checked){
+        //alert(`You have selected the second option`);
+        console.log("selected q2 a2");
+    }else if (myElement2[2].checked){
+        //alert(`You have selected the third option`);
+        console.log("selected q2 a3");
+    }else if (myElement2[3].checked){
+        //alert(`You have selected the fourth option`);
+        console.log("selected q2 a4");
+    }
+   
+     
+}
 
 
 
